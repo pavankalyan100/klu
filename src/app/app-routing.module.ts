91b048e3b8a6bb238b63b7787a82a8c48a788fc4
Pavan Kalyan <pavankalyangunda1@gmail.com>
2022-12-27 09:24:28 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,44 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DatabindComponent } from './databind/databind.component';
+import { ErrorpageComponent } from './errorpage/errorpage.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'databind',
+    component:DatabindComponent
+  },
+  {
+    path:'home',
+    component:HomeComponent
+  },
+  {
+    path:'login',
+    component:LoginComponent
+  },
+  {
+    path:'register',
+    component:RegisterComponent
+  },
+  {
+    path:'gallery',
+    component:GalleryComponent
+  },
+  {
+    path:'',
+    component:LoginComponent
+  },
+  {
+    path:'**',
+    component:ErrorpageComponent
+  },
+  
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
